483b79e7657f092cdb64943203ecac04919b6689
rohitaryal <erucix@gmail.com>
2025-08-08 05:49:37 +0530

    Added backup mode

--- cmd/same/main.go
+++ cmd/same/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"encoding/gob"
 	"flag"
 	"fmt"
+	"os"
+	"path"
+	"time"
 
+	"github.com/fatih/color"
 	"github.com/rohitaryal/same/pkg/hasher"
 	"github.com/rohitaryal/same/pkg/scanner"
 )
+
+var Color = map[string]string{
+	"ERROR":   color.RedString("[✕]"),
+	"LOADING": color.BlueString("[*]"),
+	"SUCCESS": color.GreenString("[✓]"),
+	"WARNING": color.YellowString("[✕]"),
+}
 
 var (
 	// Specifies if we should make a backup file
@@ -33,7 +45,9 @@ var (
 func main() {
 	flag.BoolVar(&backupMode, "b", true, "Initiates backup mode of a directory")
 	flag.BoolVar(&checkupMode, "c", false, "Initiates checkup mode using a backup file")
-	flag.StringVar(&backupFile, "file", "", "Path to backup file")
+
+	flag.StringVar(&backupFile, "file", fmt.Sprint("backup", time.Now().Unix()), "Path to save backup file")
+
 	flag.StringVar(&backupDirectory, "dir", "", "Directory that needs to be backed up")
 	flag.StringVar(&hashMode, "hash", "MD5", "Hash to use for integrity check")
 	flag.BoolVar(&verbose, "v", false, "Make the operation verbose")
@@ -46,6 +60,11 @@ func main() {
 
 	// Dont' forget this one
 	flag.Parse()
+
+	// Append `.same` extension
+	if path.Ext(backupFile) != ".same" {
+		backupFile += ".same"
+	}
 
 	if backupMode {
 		fmt.Println("Backing up: ", backupDirectory)
@@ -60,7 +79,14 @@ func main() {
 func backup() {
 	channel := make(chan *scanner.File, 100)
 
-	var err error
+	backup, err := os.Create(backupFile)
+	if err != nil {
+		fmt.Printf("%s Failed to create backup file.", Color["ERROR"])
+		return
+	}
+
+	defer backup.Close()
+
 	var head scanner.File
 
 	go func() {
@@ -85,10 +111,16 @@ func backup() {
 			}
 		}
 
-		fmt.Printf("\r [+] Total Scanned: %d   Saved: %d   Failure: %d", scanned, scanned-errored, errored)
+		fmt.Printf("\r%s Total Scanned: %d   Saved: %d   Failure: %d", Color["LOADING"], scanned, scanned-errored, errored)
 	}
 
-	fmt.Println("\nHead: ", head.Size)
+	fmt.Printf("\r%s Total Scanned: %d   Saved: %d   Failure: %d", Color["SUCCESS"], scanned, scanned-errored, errored)
 
-	fmt.Println()
+	encoder := gob.NewEncoder(backup)
+	if err = encoder.Encode(head); err != nil {
+		backup.Close()
+		fmt.Printf("\n%s Failed to write to backup file.\n", Color["ERROR"])
+	}
+
+	fmt.Printf("\n%s Hash backup saved to: %s\n", Color["SUCCESS"], backupFile)
 }
